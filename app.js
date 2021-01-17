b5eb28e1cc52c8456b037e047e2d78a5688bf0e3
Juliane Souza <julianesouza.pro@gmail.com>
2021-01-17 17:19:19 -0400

    adding lyrics to the front-end

--- app.js
+++ app.js
@@ -24,25 +24,26 @@ app.post("/", function(req, res){
 
     let title = req.body.title;
 
-    music.trackSearch({q:"Chet Faker - Gold", page:1, page_size:3})
+    music.trackSearch({q:title, page:1, page_size:3})
 	.then(function(data){
-		console.log(data);
+        const track_is = data.message.body.track_list[0].track.track_id;
+
+            music.trackLyrics({track_id:track_is})
+            .then(function(data){
+                const lyrics = data.message.body.lyrics.lyrics_body;
+                res.render("index", {
+                    success: true,
+                    lyrics: lyrics
+                })
+            }).catch(function(err){
+                console.log(err);
+        });
+
 	}).catch(function(err){
 		console.log(err);
+    }); 
 });
 
-    music.trackLyrics({track_id:15445219})
-        .then(function(data){
-            console.log(data.message.body.lyrics.lyrics_body);
-        }).catch(function(err){
-            console.log(err);
-    });
-
-    res.render("index", {
-        success: true
-    })
-});
-
-app.listen(2000, function(){
+app.listen(3000, function(){
     console.log("server running on port 3000");
 });
