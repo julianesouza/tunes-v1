648e8bf119c3591431315632c9224514097476a2
Juliane Souza <julianesouza.pro@gmail.com>
2021-01-17 16:53:32 -0400

    using npm module musixcmatch by c0b41 on Github

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require("ejs");
 const https = require("https");
 const {config} = require('./config.js'); 
 const mykey = config.MY_KEY;
+const music = require('musicmatch')({apikey:mykey});
 
 const app = express();
 
@@ -23,11 +24,25 @@ app.post("/", function(req, res){
 
     let title = req.body.title;
 
+    music.trackSearch({q:"Chet Faker - Gold", page:1, page_size:3})
+	.then(function(data){
+		console.log(data);
+	}).catch(function(err){
+		console.log(err);
+});
+
+    music.trackLyrics({track_id:15445219})
+        .then(function(data){
+            console.log(data.message.body.lyrics.lyrics_body);
+        }).catch(function(err){
+            console.log(err);
+    });
+
     res.render("index", {
         success: true
     })
 });
 
-app.listen(5000, function(){
+app.listen(2000, function(){
     console.log("server running on port 3000");
 });
