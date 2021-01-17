c7e3089b5f75ffb39fbe644e3a5f2e04d2f202b7
Juliane Souza <julianesouza.pro@gmail.com>
2021-01-17 16:18:07 -0400

    api calls

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const ejs = require("ejs");
 const https = require("https");
 const {config} = require('./config.js'); 
 
-var mykey = config.MY_KEY;
+const mykey = config.MY_KEY;
+const url = "https://api.musixmatch.com/ws/1.1/";
+const end_url = "&apikey=" + mykey;
 
 const app = express();
 
@@ -15,9 +17,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res){
-    res.render("index");
+    res.render("index", {
+        success: false
+    });
 });
 
+app.post("/", function(req, res){
+
+    let title = req.body.title;
+    const urlRequest = url + "track.search?format=jsonp&callback=callback&q_track=" + title + end_url;
+
+    https.get(urlRequest, function(response){
+        console.log(response.statusCode);
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    res.render("index", {
+        success: true
+    })
+});
 
 app.listen(5000, function(){
     console.log("server running on port 3000");
